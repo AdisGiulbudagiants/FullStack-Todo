3ae9562f6593fb92ac3bee3e0dd900e65bb4eab7
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-09-23 16:26:15 +0700

    Add Fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "poppins-light": ["Poppins", "sans-serif"],
+        "poppins-medium": ["Poppins", "sans-serif"],
+        "poppins-semiBold": ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        "poppins-light": 300,
+        "poppins-medium": 500,
+        "poppins-semiBold": 600,
+      },
+    },
   },
   plugins: [],
 }
