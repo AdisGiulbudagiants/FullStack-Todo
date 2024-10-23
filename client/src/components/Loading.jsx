58cfb5936ef2bc3d266d811923b44b92d7ee47e2
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-10-23 18:35:51 +0700

    Simplyfied Loading.jsx

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,14 +1,6 @@
-import loadingBg from "../assets/loading-bg.jpg"
-import Image from "./Image"
-
 const Loading = () => {
   return (
-    <div className="relative h-screen flex justify-center items-center select-none">
-      <Image
-        src={loadingBg}
-        hash="L745:+*JHXIB%}t7RiRjL#R5k;o|"
-        style="absolute w-full h-screen object-cover"
-      />
+    <div className="relative h-screen flex justify-center items-center select-none bg-cyan-700">
       <div className="absolute h-screen flex flex-col justify-center items-center z-10">
         <h1 className="font-poppins-medium text-[128px] text-white">Loading</h1>
       </div>
